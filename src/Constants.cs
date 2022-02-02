6c48af8135e4b43ed7364ed8dd27c20a74716b28
Vitalii Mural <vitalii.mural@gmail.com>
2022-02-02 12:13:06 +0200

    v1.0.0.44

--- src/Constants.cs
+++ src/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string UpdateUrl = "https://raw.githubusercontent.com/vmural/call-centre/master/version.xml";
+        public const string UpdateUrl = "https://raw.githubusercontent.com/femma-it/call-centre/master/version.xml";
         public const string ApiUrl = "https://femma.qexal.xyz";
         public const string AutoProvisioningUrl = ApiUrl + "/api/v1/microsip/configuration";
         public const string LogoutUrl = ApiUrl + "/api/v1/microsip/logout";
