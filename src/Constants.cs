5b26b2c4ccf761d481f2d0e00c3188eb61f00176
Vitalii Mural <vitalii.mural@gmail.com>
2022-10-18 17:34:57 +0300

    v1.0.0.49

--- src/Constants.cs
+++ src/Constants.cs
@@ -3,16 +3,16 @@
     public static class Constants
     {
         public const string UpdateUrl = "https://raw.githubusercontent.com/femma-it/call-centre/master/version.xml";
+        public const string Authority = "https://sso.qexal.app";
+
 #if DEBUG
         public const string ApiUrl = "https://localhost:62254/";
-        public const string Authority = "https://localhost:5011";
 
 #else
         public const string ApiUrl = "https://femma.qexal.app/module/cti/";
-        public const string Authority = "https://femma.qexal.app";
 #endif
         public const string ClientId = "microsip";
-        public const string Scope = "openid profile offline_access Qexal.ApiAPI";
+        public const string Scope = "openid offline_access Qexal.ApiAPI";
 
         public const string AutoProvisioningUrl = ApiUrl + "configuration";
     }
